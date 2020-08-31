16bf1a783b15fa80f9902179afec63fcb55b0fd3
sakiy <skyksun3@gmail.com>
2020-08-31 19:19:52 +0900

    ユーザー登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do 
+    validates :nickname, :birthday
+    validates :password, length: {minimum: 6}, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字のみを利用してください。英字、数字の両方が必要です' }
+    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+    validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ン一]+\z/, message: '全角カタカナを使用してください' }
+  end
+
 end
