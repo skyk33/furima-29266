f0c5893359a2463e3873e08d04bd89b74688226d
sakiy <skyksun3@gmail.com>
2020-09-01 18:51:24 +0900

    Rubocopによるスタイルの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname {"nickname"}
-    email {"test@example.com"}
-    password = "test01"
-    password {password}
-    password_confirmation {password}
-    family_name {"米田"}
-    first_name {"こめ"}
-    family_name_kana {"コメダ"}
-    first_name_kana {"コメ"}
-    birthday {"2000-01-11"}
+    nickname { 'nickname' }
+    email { 'test@example.com' }
+    password = 'test01'
+    password { password }
+    password_confirmation { password }
+    family_name { '米田' }
+    first_name { 'こめ' }
+    family_name_kana { 'コメダ' }
+    first_name_kana { 'コメ' }
+    birthday { '2000-01-11' }
   end
 end
