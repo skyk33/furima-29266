b701962524f394e1d6623d5d7e882d10e95b099f
sakiy <skyksun3@gmail.com>
2020-09-03 19:01:36 +0900

    商品出品(登録)の実装（画像なし）
    商品登録機能の実装（画像なし）
    - ルート、アクションの設定、バリデーション、エラー表示、ユーザログイン状態によるページ遷移

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,15 +3,16 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_charge_type
-  belongs_to_active_hash :delivery_day
+  belongs_to_active_hash :delivery_schedule
   belongs_to_active_hash :prefecture
 
   with_options presence: true do
-    validates :name, :description, :category, :condition, :delivery_charge_type, :delivery_day, :prefecture, :price
+    validates :name, :description, :category, :condition, :delivery_charge_type, :delivery_schedule, :prefecture
+    validates :price, numericality: {only_integer: true, message: 'should be numbers'}, inclusion: { in: 300..9999999, message: 'is out of price range' }
   end
 
-  with_options numericality: { other_than: 0 } do
-    validates :category_id, :condition_id, :delivery_charge_type_id, :delivery_day_id, :delivery_from_id
+  with_options numericality: { other_than: 0, message: 'is not selected' } do
+    validates :category_id, :condition_id, :delivery_charge_type_id, :delivery_schedule_id, :prefecture_id
   end
   
 end
