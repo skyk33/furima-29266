c0956fe56b6dafc2853c3e44099d907432b9c66a
sakiy <skyksun3@gmail.com>
2020-09-07 15:51:28 +0900

    Revert "商品ページのJSファイル修正"
    This reverts commit eea81049b8ae285bed16d902e738c785bb2ff7e2.

--- app/javascript/profit.js
+++ app/javascript/profit.js
@@ -1,14 +1,13 @@
 function profit() {
-  const sales_price = document.querySelector("input[name='item[price]']");
+  const sales_price = document.getElementById("item-price");
   const sales_commission = document.getElementById("add-tax-price");
   const profit_price = document.getElementById("profit");
 
-  if (sales_price != null) {
-    sales_price.addEventListener("change", () => {
-      inputValue = parseInt(sales_price.value);
-      sales_commission.innerHTML = inputValue * 0.1;
-      profit_price.innerHTML = inputValue - (sales_commission.innerHTML);
-    });
-  };
+  sales_price.addEventListener("change", () => {
+    inputValue = parseInt(sales_price.value);
+    sales_commission.innerHTML = inputValue * 0.1;
+    profit_price.innerHTML = inputValue - (sales_commission.innerHTML);
+  })
 };
+
 window.addEventListener("load", profit);
