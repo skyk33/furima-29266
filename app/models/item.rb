b380c4bb93a190d46ae6f7b1347077574a9fdd5f
sakiy <skyksun3@gmail.com>
2020-09-07 18:27:43 +0900

    Rubocopによるステイル修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,10 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :name, :description, :category, :condition, :delivery_charge_type, :delivery_schedule, :prefecture, :image
-    validates :price, numericality: {only_integer: true, message: 'should be numbers'}, inclusion: { in: 300..9999999, message: 'is out of price range' }
+    validates :price, numericality: { only_integer: true, message: 'should be numbers' }, inclusion: { in: 300..9_999_999, message: 'is out of price range' }
   end
 
   with_options numericality: { other_than: 0, message: 'is not selected' } do
     validates :category_id, :condition_id, :delivery_charge_type_id, :delivery_schedule_id, :prefecture_id
   end
-  
 end
