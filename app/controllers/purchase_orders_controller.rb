2a10049af8ed1c778e93beeeb2586a25c49d200f
sakiy <skyksun3@gmail.com>
2020-09-11 22:12:03 +0900

    購入処理の実装

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,8 +1,34 @@
 class PurchaseOrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order = PurchaseOrder.new
   end
 
   def create
+    binding.pry
+    @item = Item.find(params[:item_id])
+    @order = PurchaseOrder.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:token).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,4 +1,8 @@
 class PurchaseOrder < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  attr_accessor :token
+  validates :user_id, :item_id, presence: true
+  validates :token, presence: true
 end
