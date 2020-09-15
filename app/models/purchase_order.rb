67ec07d38abb9a33627591463b3d526cbefe5a90
sakiy <skyksun3@gmail.com>
2020-09-15 16:52:33 +0900

    コメントアウトの削除

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -2,8 +2,4 @@ class PurchaseOrder < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :delivery_address
-
-  # attr_accessor :token
-  # validates :user_id, :item_id, presence: true
-  # validates :token, presence: true
 end
